7eb2494c8d7adf5575fa55c1d8b908407cc3db3e
wxpftd <zxc2012@gmail.com>
2014-12-11 09:48:53 +0800

    Fix indent problem.

--- Bigram.py
+++ Bigram.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
 # -*- coding=utf-8 -*-
 
+LocalCh='modelData/characterFile.txt'
+LocalWords='modelData/wordsFile.txt'
+Localcorpus='corpus/corpus.txt'
+
 class Bigram:
 	'''
 	Bigram语言模型
@@ -15,12 +19,11 @@ class Bigram:
 	judgeResult=file
 
 	def __init__(self):
-		pass
-
-    def trainModel(self, corpusPath='corpus/corpus.txt'):
-        with open(corpusPath) as f:
-            for line in f:
-                line = line.strip()
+	    pass
+        def trainModel(self, corpusPath=Localcorpus):
+	    with open(corpusPath) as f:
+		for line in f:
+		    line = line.strip()
                 for i in range(0, len(line)):
                     if not character.has_key(line[i]):
                         character[line[i]] = 1
@@ -31,32 +34,36 @@ class Bigram:
                     else:
                         words[line[i:i+1]] += 1 
 
-	def modelOutput(self, characterFilePath='modelData/characterFile.txt', wordsFilePath='modelData/wordsFile.txt'):
-        with characterFile = open(characterFilePath, 'w')
-        for ch, count in character:
-            characterFile.write(ch+':'+count)
-        wordsFile = open(wordsFilePath, 'w')
-        for word, count in words:
-            wordsFile.write(word+':'+count)
+	def modelOutput(self, characterPath=LocalCh, wordsPath=LocalWords):
+	    characterFile = open(characterPath, 'w')
+            for ch, count in character:
+                characterFile.write(ch+':'+count)
+            characterFile.close()
+            wordsFile = open(wordsPath, 'w')
+            for word, count in words:
+                wordsFile.write(word+':'+count)
+            wordsFile.close()
         
 
-	def ModelLoader(self, characterFilePath='modelData/characterFile.txt', wordsFilePath='modelData/wordsFile.txt'):
-        characterFile = open(characterFilePath, 'r')
-        wordsFile = open(wordsFilePath, 'r')
-        for line in characterFile:
-            parts = line.split(':')
-            character[parts[0]] = parts[1]
-        for line in wordsFile:
-            parts = line.split(':')
-            words[parts[0]] = parts[1]
+	def ModelLoader(self, characterPath=LocalCh, wordsPath=LocalWords):
+            characterFile = open(characterPath, 'r')
+            wordsFile = open(wordsPath, 'r')
+            for line in characterFile:
+                parts = line.split(':')
+                character[parts[0]] = parts[1]
+            for line in wordsFile:
+                parts = line.split(':')
+                words[parts[0]] = parts[1]
+            characterFile.close()
+            wordsFile.close()
 
-    def getPiecePro(self, piece):
-        if character[piece]:
+        def getPiecePro(self, piece):
+	   pass
 
 
 	def calculate(self, sentence):
-		pass
+	   pass
 	
 	def TestSetProcess(self):
-		pass
+	   pass
 
